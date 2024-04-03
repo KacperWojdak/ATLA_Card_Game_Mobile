11096942ee9697470dc750e258e412b65110da41
KacperWojdak <kacper.wojdak@interia.pl>
2024-04-03 18:58:56 +0200

    ATLA v0.4.3
    Fixed cards zoom and scrolling (hope so)

--- ATLA_CardGame/Assets/Scripts/Cards/CardZoom.cs
+++ ATLA_CardGame/Assets/Scripts/Cards/CardZoom.cs
@@ -9,81 +9,72 @@ public class CardZoom : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
     private Vector3 originalPosition;
     private Transform originalParent;
 
+    private bool isZoomed = false;
+    private Coroutine holdCoroutine;
+
     public RectTransform screenCardView;
     public ScrollRect scrollRect;
+    public float zoomScale = 2f;
+    public float zoomDelay = 1f;
+    public float moveSpeed = 15f;
 
-    public float zoomScale = 3f;
-    public float moveSpeed = 15f; 
-
-    private bool isZoomed = false;
-    private Coroutine zoomCoroutine;
     private int originalUIIndex;
 
     void Start()
     {
         originalScale = transform.localScale;
-        scrollRect = GetComponentInParent<ScrollRect>();
     }
 
     public void OnPointerDown(PointerEventData eventData)
     {
-        scrollRect.enabled = false;
-
-        zoomCoroutine = StartCoroutine(DelayedAction(0.5f, () =>
-        {
-            if (!isZoomed)
-            {
-                ZoomCard();
-            }
-        }));
+        holdCoroutine = StartCoroutine(HoldToZoom());
     }
 
     public void OnPointerUp(PointerEventData eventData)
     {
-        scrollRect.enabled = true;
-
-        if (zoomCoroutine != null)
+        if (holdCoroutine != null)
         {
-            StopCoroutine(zoomCoroutine);
-            zoomCoroutine = null;
+            StopCoroutine(holdCoroutine);
+            holdCoroutine = null;
         }
 
-        if (isZoomed)
-        {
-            UnzoomCard();
-        }
+        if (isZoomed) ResetZoom();
     }
 
     private void ZoomCard()
     {
-        isZoomed = true;
         originalPosition = transform.position;
         originalParent = transform.parent;
         originalUIIndex = transform.GetSiblingIndex();
 
-        transform.SetParent(screenCardView);
-        transform.SetAsLastSibling();
-
-        StartCoroutine(MoveToPosition(screenCardView.position));
-        transform.localScale = originalScale * zoomScale;
+        if (!isZoomed)
+        {
+            transform.SetParent(screenCardView);
+            transform.SetAsLastSibling();
+            StartCoroutine(MoveToPosition(screenCardView.position));
+            transform.localScale = originalScale * zoomScale;
+            isZoomed = true;
+            scrollRect.enabled = false;
+        }
     }
 
-    private void UnzoomCard()
+    private void ResetZoom()
     {
         isZoomed = false;
+        scrollRect.enabled = true;
+
         StartCoroutine(MoveToPosition(originalPosition, () =>
         {
             transform.SetParent(originalParent);
             transform.localScale = originalScale;
             transform.SetSiblingIndex(originalUIIndex);
         }));
-
     }
 
-    private IEnumerator DelayedAction(float delay, System.Action action)
+    private IEnumerator HoldToZoom()
     {
-        yield return new WaitForSeconds(delay);
-        action?.Invoke();
+        yield return new WaitForSeconds(zoomDelay);
+        ZoomCard();
     }
 
     private IEnumerator MoveToPosition(Vector3 target, System.Action onComplete = null)
